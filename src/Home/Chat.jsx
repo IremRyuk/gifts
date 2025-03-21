0068b216e4ba4749c6eee541ebe7e533537b0241
IremRyuk <IremRyuk>
2025-03-21 17:07:48 +0400

    added details

--- src/Home/Chat.jsx
+++ src/Home/Chat.jsx
@@ -1,10 +1,29 @@
 import ChatIcon from '@mui/icons-material/Chat';
 import '../SCSS/Home/home.css'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import {SpeedDial,SpeedDialAction} from '@mui/material'
 
 export default function Chat() {
+  const actions = [
+    { icon: <WhatsAppIcon />, name: 'Whatsappp' },
+    { icon: <LocalPhoneIcon />, name: 'Phone' },
+    { icon: <FacebookIcon />, name: 'Facebook' },
+  ]
   return (
-    <div className='chat'>
-        <ChatIcon fontSize='large' sx={{color:"white"}}/>
-    </div>
+        <SpeedDial
+        ariaLabel="SpeedDial openIcon example"
+        sx={{ position: 'fixed', bottom:'5%', right:'5%'}}
+        icon={<ChatIcon openIcon={<ChatIcon sx={{bgcolor:'green',transition:'0.2s','&:hover':{scale: '1.15'}}}/>} />}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            name={action.name}
+          />
+        ))}
+      </SpeedDial>
   )
 }
--- src/Nav/LeftNav.jsx
+++ src/Nav/LeftNav.jsx
@@ -5,11 +5,28 @@ import Man from '../Photoes/man.png'
 import HelpIcon from '@mui/icons-material/Help';
 import {useDispatch} from 'react-redux'
 import {SearchAction} from '../Redux/Action/SearchAction'
+import {Modal,Box ,Typography} from '@mui/material'
+import { useState } from 'react';
 export default function LeftNav(){
         let dispatch = useDispatch()
+        const [open,setOpen] = useState(false)
         const Local = (word) => {
                 dispatch(SearchAction(word))
         }
+        const style = {
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width:{xs:'200px',sm:'400px',lg:'700px'},
+                height:{xs:'100px',sm:'400px',lg:'500px'},
+                bgcolor: 'background.paper',
+                border: 'none',
+                outline:'none',
+                borderRadius:'2.4mm',
+                boxShadow: 24,
+                p: 4,
+              };
     return(
         <div className="leftNav">
         <div className="menu">
@@ -20,7 +37,22 @@ export default function LeftNav(){
 <img src={Man} alt="man" className='imgprofile' onClick={()=>Local('man')} />
         </div>
         <div className='help'>
-<HelpIcon className='menuBtn' fontSize="large"/>
+<HelpIcon className='menuBtn' fontSize="large" onClick={()=>setOpen(e=>!e)}/>
+<Modal
+        open={open}
+        onClose={()=>setOpen(e=>!e)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <center><Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            თაითლი
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            აღწერა
+          </Typography>
+        </Box></center>
+      </Modal>
         </div>
         </div>
     )
